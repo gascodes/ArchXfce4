9fe2cca6ff8e43d90d5d25e3059704aa40fface6
Gaston <93546109+gascodes@users.noreply.github.com>
2022-08-21 23:57:02 -0300

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -4,3 +4,5 @@ sudo pacman -S --needed -< pkglist.txt
 sudo systemctl enable lightdm.service
 sudo systemctl enable bluetooth.service
 xdg-user-dirs-update
+
+#sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
